b0a585067a2327fbbc9c5d75a99fab940d284827
zimoyin <2556608754@qq.com>
2022-11-29 19:26:05 +0800

    fix: The CookieUpdeate resource was not properly closed

--- src/test/kotlin/main/TestRun.kt
+++ src/test/kotlin/main/TestRun.kt
@@ -1,15 +1,13 @@
 package main
 
+import ink.bluecloud.service.clientservice.account.cookie.CookieUpdate
 import ink.bluecloud.service.clientservice.video.info.VideoInfo
 import ink.bluecloud.service.clientservice.video.stream.VideoStream
 
 class TestRun {
     suspend fun run() {
-        val (video,audio) = VideoStream().getVideoStream("BV1sP411g7PZ", 892292717)
-        println(audio.get().url)
-        println(video.get().url)
-
-
-        println(VideoInfo().getVideoInfo("BV1Mg411i74w").toString())
+        for (cookie in CookieUpdate().getCookieStore().getBiliCookieStore()) {
+            println(cookie)
+        }
     }
 }
